b6b08f988a4f6c6dd4f51c3a07868cfedf487829
Giovanna González <GiovaWolf@imac-de-giovanna.local>
2016-07-24 21:08:14 -0500

    Tarea 1

--- Tarea1.playground/Contents.swift
+++ Tarea1.playground/Contents.swift
@@ -2,4 +2,22 @@
 
 import UIKit
 
+for i in 0...100{
+    if i % 2 == 0{
+        print ("\(i) es par")
+    }
+    if i % 5 == 0 {
+        
+        print ("\(i) bingo")
+    }
+    if i % 2 != 0 {
+        
+        print ("\(i) impar")
+    }
+    if i < 41 && i > 29 {
+        
+        print ("\(i) viva Swift")
+    }
+
+}
 
