1c046ebc12baa1adee6a8d30c85ff0b37cd147e2
Luis <lufum88@gmail.com>
2021-05-28 21:00:23 -0500

    la cague

--- PrevisMask/scripts/previsMask.py
+++ PrevisMask/scripts/previsMask.py
@@ -3,7 +3,7 @@ import datetime
 import maya.cmds as cmds
 import zshotmask_ui
 
-class previsMask():    
+class previsMask():
     def __init__(self):
         self.crateMask()
         self.updateDate()
@@ -15,7 +15,14 @@ class previsMask():
         for mp in cmds.getPanel(type="modelPanel"):
             if cmds.modelEditor(mp, q=1, av=1):
                 cur_mp = mp
-        cur_cam= cmds.modelEditor(cur_mp, q=1, av =1, cam=1) 
+                break
+
+        cur_cam= cmds.modelEditor(cur_mp, q=1, av =1, cam=1)
+        cur_cam= cmds.modelEditor(cur_mp, q=1, av =1, cam=1)
+        if cmds.objectType(cur_cam) != "transform":
+            par = cmds.listRelatives(cur_cam, p=1, f=1)
+            cur_cam = par[0]
+        
         file_=os.path.basename(cmds.file(q=True, sn=True))
         
         #Info
@@ -51,19 +58,29 @@ class previsMask():
                                 $cur_mp = $mp;
                             }
                         }
-                        string $cur_cam= `modelEditor -q -cam $cur_mp`;
-                        float $cam_rotate[]=  `xform  -q -ro $cur_cam`;
-                        float $cam_translate[]=  `xform  -q -ws -t $cur_cam`;
+                        string $cur_cam= `modelEditor -q -cam $cur_mp`; 
+                        if (`objectType $cur_cam` != "transform"){
+                            string $par [] = `listRelatives -p -f $cur_cam `;
+                            $cur_cam = $par[0];
+												
+                        }
+						string $name = $cur_cam;
+						string $tokenized[];
+						tokenize $name "|" $tokenized;
+						string $final_name = $tokenized[size($tokenized) - 1];						
+                        float $cam_rotate =  `getAttr ($cur_cam + ".rotateX")`;
+                        float $cam_translate =   `getAttr ($cur_cam + ".rotateX")`;
                         float $lens_info= `camera -q -fl $cur_cam`;
                         float $truc_lens= trunc($lens_info * pow(10, 2))/pow(10,2);
-                        float $cam_angle= trunc($cam_rotate[0] * pow(10, 2))/pow(10,2);
-                        float $cam_height= trunc($cam_translate[1] * pow(10, 2))/pow(10,2);
+                        float $cam_angle= trunc($cam_rotate * pow(10, 2))/pow(10,2);
+                        float $cam_height= trunc($cam_translate * pow(10, 2))/pow(10,2);
                         string $pad_angle= `python ("'%04d' % "+$cam_angle)`;
                         string $pad_height= `python ("'%05d' % "+$cam_height)`;
                         string $cam_info= "h: " + $pad_height + "  tilt: " + $cam_angle;
                         string $lens_mm= "lens: " + $truc_lens +" mm";
+						
                     
-                        setAttr -type "string" zshotmask_shape.topCenterText $cur_cam;
+                        setAttr -type "string" zshotmask_shape.topCenterText $final_name;
                         setAttr -type "string" zshotmask_shape.bottomLeftText $cam_info;
                         setAttr -type "string" zshotmask_shape.bottomCenterText $lens_mm;
                         setAttr -type "string" zshotmask_shape.camera cur_cam""")
