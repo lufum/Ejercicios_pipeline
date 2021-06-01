828f7990f3e0d2d4407e785c03edb6092bdb5637
Luis <lufum88@gmail.com>
2021-05-31 19:19:44 -0500

    Previs funciona con Ubercam

--- PrevisMask/scripts/previsMask.py
+++ PrevisMask/scripts/previsMask.py
@@ -53,37 +53,75 @@ class previsMask():
                         
                         """
                         string $cur_mp;
+                        
                         for ($mp in `getPanel -typ "modelPanel"`){
                             if (`modelEditor -q -av $mp`){
                                 $cur_mp = $mp;
                             }
                         }
+                        
                         string $cur_cam= `modelEditor -q -cam $cur_mp`; 
                         if (`objectType $cur_cam` != "transform"){
                             string $par [] = `listRelatives -p -f $cur_cam `;
-                            $cur_cam = $par[0];
-												
+                            $cur_cam = $par[0];						
                         }
+                        
 						string $name = $cur_cam;
 						string $tokenized[];
 						tokenize $name "|" $tokenized;
-						string $final_name = $tokenized[size($tokenized) - 1];						
+						string $final_name = $tokenized[size($tokenized) - 1];
+
+                        if ($cur_cam == "ubercam"){
+                            float $uber_rotate = `getAttr ($cur_cam + ".rotateX")`;
+                            float $uber_translate = `getAttr ($cur_cam + ".translateY")`;
+                            string $cam_ls[]= `ls  -type "camera"`;
+                            float $max_dif = 0.2;
+                            for ($cam in $cam_ls) {
+                                $cam_t= `listRelatives -p -f $cam`;
+                                
+                                if ($cam_t[0] != "|ubercam"){                           
+                                    float $cam_rotate= `getAttr ($cam_t[0] + ".rotateX")`;
+                                    float $cam_translate= `getAttr ($cam_t[0] + ".translateY")`;
+                                    if ($cam_rotate < 0) { $cam_rotate = $cam_rotate*-1; }
+                                    if ($cam_translate < 0) { $cam_translate = $cam_translate*-1; }
+                                    if ($uber_rotate < 0) { $uber_rotate = $uber_rotate*-1; }
+                                    if ($uber_translate < 0) { $uber_translate = $uber_translate*-1; }                                    
+                                    float $dif_R = $uber_rotate - $cam_rotate; 
+                                    if ($dif_R < 0) { $dif_R = $dif_R*-1; }
+                                    float $dif_t = $uber_translate - $cam_translate;
+                                    if ($dif_t < 0) { $dif_t = $dif_t*-1; }
+                                    
+                                    if ($dif_R <= $max_dif && $dif_t <= $max_dif ){
+                                        string $name = $cam_t[0];
+                                        string $tokenized[];
+                                        tokenize $name "|" $tokenized;
+                                        string $final_name = $tokenized[size($tokenized) - 1];
+                                        setAttr -type "string" zshotmask_shape.topCenterText $final_name;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        else{
+                            setAttr -type "string" zshotmask_shape.topCenterText $final_name;                   
+                        }
+                        
                         float $cam_rotate =  `getAttr ($cur_cam + ".rotateX")`;
-                        float $cam_translate =   `getAttr ($cur_cam + ".rotateX")`;
+                        float $cam_translate =   `getAttr ($cur_cam + ".translateY")`;
                         float $lens_info= `camera -q -fl $cur_cam`;
                         float $truc_lens= trunc($lens_info * pow(10, 2))/pow(10,2);
                         float $cam_angle= trunc($cam_rotate * pow(10, 2))/pow(10,2);
                         float $cam_height= trunc($cam_translate * pow(10, 2))/pow(10,2);
-                        string $pad_angle= `python ("'%04d' % "+$cam_angle)`;
-                        string $pad_height= `python ("'%05d' % "+$cam_height)`;
+                        string $pad_angle= `python ("'%04.2f' % "+$cam_angle)`;
+                        string $pad_height= `python ("'%05.2f' % "+$cam_height)`;
                         string $cam_info= "h: " + $pad_height + "  tilt: " + $cam_angle;
                         string $lens_mm= "lens: " + $truc_lens +" mm";
-						
-                    
-                        setAttr -type "string" zshotmask_shape.topCenterText $final_name;
+
                         setAttr -type "string" zshotmask_shape.bottomLeftText $cam_info;
                         setAttr -type "string" zshotmask_shape.bottomCenterText $lens_mm;
-                        setAttr -type "string" zshotmask_shape.camera cur_cam""")
+                        setAttr -type "string" zshotmask_shape.camera cur_cam
+                    """)
+                        
     
     def updateDate(self):
         fecha= datetime.datetime.now()
@@ -92,4 +130,4 @@ class previsMask():
         year= str(fecha.year).zfill(2)
         hour= str(fecha.hour).zfill(2)+':'+str(fecha.minute).zfill(2)
         fecha_disp= year+'/'+month+'/'+day+' - '+hour
-        cmds.setAttr( "zshotmask_shape.topRightText", fecha_disp, type="string")
+        cmds.setAttr( "zshotmask_shape.topRightText", fecha_disp, type="string")l
